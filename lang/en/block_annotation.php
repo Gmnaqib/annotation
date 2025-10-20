50460530eecebb6c4d9bb3dab4c38a6dd200b1b0
Gmnaqib <gumilarmn16@gmail.com>
2025-10-21 00:07:03 +0700

    first commit

---
+++ lang/en/block_annotation.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     block_annotation
+ * @category    string
+ * @copyright   2025 Gumilar <gumilarmn@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['annotation:addinstance'] = 'Add a new Annotation block';
+$string['annotation:myaddinstance'] = 'Add a new Annotation block to Dashboard';
+$string['errorloading'] = 'Failed to fetch annotations from API.';
+$string['loading'] = 'Loading annotations...';
+$string['pluginname'] = 'Annotation Block';
+$string['privacy:metadata'] = 'The Annotation block fetches text modules from an external API but does not store user data locally.';
+$string['settings'] = 'Annotation Settings';
+$string['viewannotation'] = 'View annotation content fetched from external source';
